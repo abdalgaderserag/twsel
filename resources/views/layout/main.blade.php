581debe047048a01f6f75d4348eaaf6dfc2bcc51
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-06-19 22:14:20 +0200

    styling the side section

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,26 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/dist/css/app.css">
 
 </head>
 <body>
-<div id="main">
-    <div>
+<div class="flex" id="main">
+    <div id="side-section">
         @include('layout.side')
     </div>
 
 
-    <div>
+    <div id="main-section">
         @yield('main')
     </div>
 
 </div>
-<script src="/js/bootstrap.min.js"></script>
 <script>
-    const showDetails = () => {
-        console.log(e);
+    function resize (){
+        const pageHeight = window.innerHeight;
+        const s = document.getElementById('side-section')
+        s.style.height = pageHeight + 'px';
+
+        const m = document.getElementById('main-section')
+        m.style.maxHeight = pageHeight + 'px';
+        m.style.height = pageHeight + 'px';
     }
+    window.onload = resize()
 </script>
 </body>
 </html>
--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -1,32 +1,61 @@
 @php($user = \Illuminate\Support\Facades\Auth::user())
-<div>
-    <span>{{ $user->name }}</span>
-    <span>
-            @if($user->isDriver())
-            driver
-        @elseif($user->isUser())
-            store
-        @endif
+<div class="header flex">
+    <img id="avatar" src="./images/avatar.png">
+    <div id="user-name">
+        {{ $user->name }} <br>
+        <span>
+        @if($user->isDriver())
+                driver
+            @elseif($user->isUser())
+                store
+            @endif
         </span>
+    </div>
+
 </div>
 
-<ul>
-    <li>
-        <a href="{{ route('orders.index') }}">Orders</a>
-    </li>
+<div id="side-selections">
+    <div>
+        <a href="{{ route('orders.index') }}">
+            <img src="/images/hand.svg">
+            Orders
+        </a>
+        <div style="border-left: white 2px solid;margin: 12px;padding-left: 10px">
+            <a href="#">New!</a>
+            <a href="#">Near me</a>
+            <a href="#">Delivered</a>
+        </div>
+    </div>
     @if($user->isDriver())
-        <li>
-            <a class="accordion-header" href="{{ route('deliver.index') }}">delivers</a>
-        </li>
-        <li>
-            <a href="#">wage</a>
-        </li>
+        <div>
+            <a href="{{ route('deliver.index') }}">
+                <img src="/images/item.svg">
+                delivers
+            </a>
+            <div style="border-left: white 2px solid;margin: 12px;padding-left: 10px">
+                <a href="#">On-going</a>
+                <a href="#">Canceled</a>
+                <a href="#">Delayed</a>
+            </div>
+        </div>
+        <div>
+            <a href="#">
+                <img src="/images/info.svg">
+                wage
+            </a>
+        </div>
     @endif
 
-    <li>
-        <a href="#">edit profile</a>
-    </li>
-    <li>
-        <a href="{{ route('logout') }}">logout</a>
-    </li>
-</ul>
+    <div>
+        <a href="#">
+            <img src="/images/edit.svg">
+            edit profile
+        </a>
+    </div>
+    <div>
+        <a href="{{ route('logout') }}">
+            <img src="/images/delete.svg">
+            logout
+        </a>
+    </div>
+</div>
